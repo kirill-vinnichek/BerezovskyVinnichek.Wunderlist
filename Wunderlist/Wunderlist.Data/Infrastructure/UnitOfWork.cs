9841ce1f8d30ffaba11f1e23170ab38234bfe546
kirill-vinnichek <kirill.vinnichek@gmail.com>
2016-02-16 17:42:03 +0300

    Add some architecture features

---
+++ Wunderlist/Wunderlist.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDatabaseFactory databaseFactory;
+        private WunderlistContext dataContext;
+
+        public UnitOfWork(IDatabaseFactory databaseFactory)
+        {
+            this.databaseFactory = databaseFactory;
+        }
+
+        protected WunderlistContext DataContext
+        {
+            get { return dataContext ?? (dataContext = databaseFactory.Context); }
+        }
+
+        public void Commit()
+        {
+            DataContext.SaveChanges();
+        }
+    }
+}
---
+++ Wunderlist/Wunderlist.Data/WunderlistContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Wunderlist.Models;
+
+namespace SignalR.Data
+{
+    public class WunderlistContext : IdentityDbContext<AppUser>
+    {
+
+        public WunderlistContext() : base("WunderlistConnection")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<WunderlistContext>());
+        }
+
+      
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<IncludeMetadataConvention>();
+        }
+
+
+        public virtual void Commint()
+        {
+            base.SaveChanges();
+        }
+
+    }
+}
