9282ecfda6a6da6338524b843e2a21500bb4b4ac
kirill-vinnichek <kirill.vinnichek@gmail.com>
2016-02-18 12:24:49 +0300

    Rename

--- Wunderlist/Wunderlist.Models/User.cs
+++ Wunderlist/Wunderlist.Models/User.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
 
 namespace Wunderlist.Models
 {
-    public class AppUser : IUser<int>
+    public class User : IUser
     {       
-        public int Id { get; }
+        public string Id { get; }
         public string UserName { get; set; }
         public string UserEmail { get; set; }
 
         public string UserPassword { get; set; }
         public int UserProfileId { get; set; }
+
+        public virtual ICollection<Role> UserRoles{ get; set; }
+
+
     }
 }
