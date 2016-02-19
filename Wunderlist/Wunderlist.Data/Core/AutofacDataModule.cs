b078d21f5350bbbcf7d1646817159ef3722283e5
kirill-vinnichek <kirill.vinnichek@gmail.com>
2016-02-19 14:18:52 +0300

    Implement Autofac modules with dependences for each layer. Delete reference from Web to Data

---
+++ Wunderlist/Wunderlist.Data/Core/AutofacDataModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wunderlist.Data.Infrastructure;
+using Wunderlist.Data.Repositories;
+
+namespace Wunderlist.Data.Core
+{
+   public class AutofacDataModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DatabaseFactory>().As<IDatabaseFactory>().InstancePerRequest();
+            builder.RegisterAssemblyTypes(typeof(UserRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            base.Load(builder);
+        }
+    }
+}
---
+++ Wunderlist/Wunderlist.Service/Core/AutofacServiceModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wunderlist.Data.Core;
+using Wunderlist.Service.Services;
+
+namespace Wunderlist.Service.Core
+{
+   public class AutofacServiceModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new AutofacDataModule());
+
+            builder.RegisterAssemblyTypes(typeof(UserService).Assembly)
+             .Where(t => t.Name.EndsWith("Service"))
+             .AsImplementedInterfaces().InstancePerRequest();
+
+            base.Load(builder);
+        }
+    }
+}
